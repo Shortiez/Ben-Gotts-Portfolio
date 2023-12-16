49d00c7ffbb5ea6787d7142e4872cb776b66e5df
Shortiez <54028203+Shortiez@users.noreply.github.com>
2023-12-16 12:58:56 +0000

    Updated config

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === "production" ? "/Ben-Gotts-Portfolio" : "",
+			base: process.env.NODE_ENV === "production" ? "Ben-Gotts-Portfolio" : "",
 		},
 	},
 	preprocess: vitePreprocess()
